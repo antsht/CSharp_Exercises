6c65b17d1efa89607deab47b5ad4366e01414091
Anton Shtern <antsht@gmail.com>
2025-04-16 08:28:20 +0300

    Initialize new console application with multiple projects and add .gitignore file. Implement features for compressing streams, handling environment variables, and serializing data in JSON and XML formats. Include database setup for EF Core and sample data files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿SectionTitle("About Samantah Jones");
+string name = "Samantah Jones";
+
+int lengthOfFirst = name.IndexOf(' ');
+int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+string firstName = name.Substring(
+    startIndex: 0,
+    length: lengthOfFirst);
+
+string lastName = name.Substring(
+    startIndex: name.Length - lengthOfLast,
+    length: lengthOfLast);
+
+Console.WriteLine($"First: {firstName}, Last: {lastName}");
+
+ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = nameAsSpan[0..lengthOfFirst];
+ReadOnlySpan<char> lastNameSpan = nameAsSpan[^lengthOfLast..];
+
+Console.WriteLine($"First: {firstNameSpan} Last: {lastNameSpan}");
+
+SectionTitle("Compressing streams");
+Compress(algorithm: "gzip");
+Compress(algorithm: "brotli");
---
+++ WorkingWithEnvVars/Program.cs
@@ -0,0 +1,12 @@
+﻿SectionTitle("Reading all environment variables for process");
+IDictionary vars = GetEnvironmentVariables();
+DictionaryToTable(vars);
+
+SectionTitle("Reading all environment variables for machine");
+IDictionary varsMachine = GetEnvironmentVariables(
+EnvironmentVariableTarget.Machine);
+DictionaryToTable(varsMachine);
+SectionTitle("Reading all environment variables for user");
+IDictionary varsUser = GetEnvironmentVariables(
+EnvironmentVariableTarget.User);
+DictionaryToTable(varsUser);
